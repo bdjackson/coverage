b538513cb03aae1808b389aa8ab92444d8904496
Brett Jackson <bjack3@gmail.com>
2012-06-19 09:24:40 +0200

    up

--- coverage_probability.py
+++ coverage_probability.py
@@ -45,16 +45,28 @@ def generate_poisson_templates(s):
 def get_poisson_prob(test_stat, mean):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # prob = mean^test_stat * exp(-mean)/(test_stat!)
-    prob = mean**test_stat * math.exp(-mean)/(math.factorial(test_stat))
+    # print '---' 
+    # print '\t%s' % (mean**test_stat)
+    # print '\t%s' % (math.exp(-mean))
+    # print '\t%s' % (math.factorial(test_stat))
+    # print '\t%s' % (math.exp(-mean)/(math.factorial(test_stat)))
+    # print '\t%s' % (mean**test_stat * math.exp(-mean)/(math.factorial(test_stat)))
+    prob = float(mean)**test_stat * math.exp(-float(mean))/float(math.factorial(test_stat))
+    # print '\ttest stat: %s - mean: %s - prob: %s' % (test_stat, mean, prob)
     return prob
 
 # -----------------------------------------------------------------------------
 def get_cumulative_prob(test_stat, mean):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    # prob = mean^test_stat * exp(-mean)/(test_stat!)
+    # print '========================================='
+    # print 'cum prob of %s' % test_stat
+    # prob = mean^test_stat * exp(-mean)/(test_stat)
     cum_prob = 0.
-    for i in xrange(test_stat+1):
-        cum_prob += get_poisson_prob(i, mean)
+    t = 0
+    while t <= test_stat:
+        cum_prob += get_poisson_prob(t, mean)
+        t += 1
+    # print 'cum prob: %s' % cum_prob
     return cum_prob
 
 # -----------------------------------------------------------------------------
@@ -80,34 +92,37 @@ def central_interval(t, points_per_bin):
 
     # search for upper bound
     i = -1
+    # print 'searching for upper bound -- t: %s' % t
     while u == -1:
         i += 1
-        # test_paramer = i*poisson_template_spacing
-        test_paramer = i
-        # test_prob = get_poisson_prob(t, test_paramer)
+        test_param= i*poisson_template_spacing
+        # test_param = i
+        # test_prob = get_poisson_prob(t, test_param)
 
-        cumulative_prob = get_cumulative_prob(i, t)
+        # cumulative_prob = get_cumulative_prob(test_param, t)
+        cumulative_prob = get_cumulative_prob(t, test_param)
 
         # if cumulative_prob > 0.16:
-        if cumulative_prob < 0.84:
-            u = test_paramer
-
-        u += 1
+        if cumulative_prob < 0.16:
+        # if cumulative_prob > 0.84:
+            u = test_param
 
     # search for lower bound
+    # print 'searching for upper bound -- t: %s' % t
     i = -1
     while l == -1:
         i += 1
-        # test_paramer = i*poisson_template_spacing
-        test_paramer = i
-        # test_prob = get_poisson_prob(t, test_paramer)
+        test_param = i*poisson_template_spacing
+        # test_param = i
+        # test_prob = get_poisson_prob(t, test_param)
 
-        cumulative_prob = get_cumulative_prob(i, t)
+        # cumulative_prob = get_cumulative_prob(test_param, t)
+        cumulative_prob = get_cumulative_prob(t, test_param)
 
-        if cumulative_prob < 0.16:
-            l = test_paramer
-
-        l += 1
+        # if cumulative_prob > 0.16:
+        # if cumulative_prob > 0.84:
+        if cumulative_prob < 0.84:
+            l = test_param - 2*poisson_template_spacing
 
     return (l, u)
 
@@ -134,8 +149,8 @@ def plot_coverage_probability(s_max, points_per_bin, n_trials):
     num_points = s_max*points_per_bin + 1
     sample_points = range(num_points)
     sample_points = [float(x)/points_per_bin for x in sample_points]
-    print 'sample_points: %s' % sample_points
-    print 'points_per_bin: %s' % points_per_bin
+    # print 'sample_points: %s' % sample_points
+    # print 'points_per_bin: %s' % points_per_bin
 
     l_profile = {}
     u_profile = {}
@@ -154,8 +169,8 @@ def plot_coverage_probability(s_max, points_per_bin, n_trials):
             for ig in prob:
                 this_gen = interval_generators[ig]
                 l,u = this_gen(t, points_per_bin)
-                print '------------------------------------------------------'
-                print 's; %s - t: %s - l: %s - u: %s' % (s,t,l,u)
+                # print '------------------------------------------------------'
+                # print 's; %s - t: %s - l: %s - u: %s' % (s,t,l,u)
                 l_profile[ig][s_itr] += float(l)/n_trials
                 u_profile[ig][s_itr] += float(u)/n_trials
                 if s >= l and s <= u:
@@ -219,7 +234,7 @@ def draw_profiles(l_profile, u_profile, sample_points):
         print '\tl profile (%d): %s' % (len(l_profile[key]), l_profile[key])
         print '\tu profile (%d): %s' % (len(u_profile[key]), u_profile[key])
         c = ROOT.TCanvas('profiles_%s' % key)
-        frame = ROOT.TH2F( 'dummy_frame_profile'
+        frame = ROOT.TH2F( 'dummy_frame_profile__%s' % key
                          , 'average profiles for %s ; Observed range ; Poisson parameter' % key
                          , 1, 0, sample_points[-1]+0.5
                          , 1, 0, 2*sample_points[-1]
@@ -262,7 +277,7 @@ def main():
     # plot_coverage_probability(20, 10, 1000)
     # plot_coverage_probability(10, 10, 1000)
     # plot_coverage_probability(3, 10, 1000)
-    plot_coverage_probability(5, 2, 100)
+    plot_coverage_probability(10, 2, 1000)
 
 # =============================================================================
 if __name__ == '__main__':
